cacc70e8839017de0b3455b9fcc0b815dd157b9d
Unknown <naoyat22@gmail.com>
2018-07-23 17:02:26 +0900

    リンゴと爆弾３

--- AppleCatch/Assets/GameDirector.cs
+++ AppleCatch/Assets/GameDirector.cs
@@ -6,9 +6,10 @@ using UnityEngine.UI;
 public class GameDirector : MonoBehaviour {
 
     GameObject timerText;
-    float time = 60.0f;
+    float time = 30.0f;
     GameObject pointText;
     int point = 0;
+    GameObject generator;
 
     public void GetApple()
     {
@@ -24,12 +25,38 @@ public class GameDirector : MonoBehaviour {
 	void Start () {
         this.timerText = GameObject.Find("Time");
         this.pointText = GameObject.Find("Point");
+        this.generator = GameObject.Find("ItemGenerator");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         this.time -= Time.deltaTime;
+
+        if (this.time < 0)
+        {
+            this.time = 0;
+            this.generator.GetComponent<ItemGenerator>().SetParameter(10000.0f, 0, 0);
+        }
+        else if (0 <= this.time && this.time < 5)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.9f, -0.04f, 3);
+        }
+        else if (0 <= this.time && this.time < 10)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.4f, -0.06f, 6);
+        }
+        else if (0 <= this.time && this.time < 20)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -0.04f, 4);
+        }
+        else if (0 <= this.time && this.time < 30)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(1.0f, -0.03f, 2);
+        }
+
         this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
         this.pointText.GetComponent<Text>().text = this.point.ToString() + "point";
 	}
+
+
 }
